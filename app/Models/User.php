342ff1b70b82e96de9d78d5a18bba19f215641b1
Edimilson Queiroz <edimilsonqueiroz681@gmail.com>
2024-07-24 20:01:24 -0300

    ok

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -42,4 +43,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany(Permission::class);
+    }
 }
---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+class UserRepository
+{
+    public function __construct(protected User $user)
+    {
+        
+    }
+
+    public function getPaginateUsers(int $totalPerPage = 15, int $page = 1, string $filter = ''): LengthAwarePaginator
+    {
+        return $this->user->where(function ($query) use ($filter) {
+            if($filter != ''){
+                $query->where('name','LIKE', "%{$filter}%");
+            }
+        })->paginate($totalPerPage, ['*'], 'page',$page);
+    }
+}
